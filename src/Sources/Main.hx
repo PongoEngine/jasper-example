1b9ed14ffaefd4ad3f211ba4383377b78bd8a177
Jeremy <cream@sourcre.am>
2018-01-12 16:31:49 -0800

    remove rectangle

--- src/Sources/Main.hx
+++ src/Sources/Main.hx
@@ -31,19 +31,19 @@ class Main {
         var _height :Float = 400;
         var solver = new Solver();
 
-        var window = new Rectangle(0xff00ff00);
-        solver.addConstraint(window.x == 0);
-        solver.addConstraint(window.y == 0);
+        var window = new Rectangle(0xff444444);
+        solver.addConstraint(window.x == 10);
+        solver.addConstraint(window.y == 10);
         solver.addConstraint((window.width == _width) | Strength.WEAK);
         solver.addConstraint((window.height == _height) | Strength.WEAK);
 
-        var leftChild = new Rectangle(0xfff0ff00);
+        var leftChild = new Rectangle(0xffaaaaaa);
         solver.addConstraint(leftChild.x == window.x + 10);
         solver.addConstraint(leftChild.y == window.y + 10);
         solver.addConstraint(leftChild.width == (window.width/2 - 20));
         solver.addConstraint(leftChild.height == (window.height - 20));
 
-        var rightChild = new Rectangle(0xfff0ffff);
+        var rightChild = new Rectangle(0xffdddddd);
         solver.addConstraint(rightChild.x == (window.x + window.width) - (window.width/2 - 10));
         solver.addConstraint(rightChild.y == window.y + 10);
         solver.addConstraint(rightChild.width == (window.width/2 - 20));
@@ -54,8 +54,6 @@ class Main {
         solver.addConstraint(centerChild.y == window.height/4);
         solver.addConstraint(centerChild.width == (window.width/2));
         solver.addConstraint(centerChild.height == (window.height/2));
-        
-
 
         _rectangles = 
             [ window
@@ -71,27 +69,25 @@ class Main {
             System.notifyOnRender(render);
 
             var _isDown :Bool = false;
-            kha.input.Mouse.get().notify(function(a,x,y) {
+            kha.input.Mouse.get().notify(function(b,x,y) {
                 _isDown = true;
-            }, function(a,x,y) {
+            }, function(b,x,y) {
                 _isDown = false;
-
-            }, function(x,y,c,d) {
+            }, function(x,y,cX,cY) {
                 if(_isDown) {
                     solver.suggestValue(window.width, x);
                     solver.suggestValue(window.height, y);
                     solver.updateVariables();
                 }
                 
-            }, function(a) {
-
+            }, function(c) {
             });
         });
     }
 
     public static function render(framebuffer: kha.Framebuffer): Void 
     {
-        framebuffer.g2.begin();
+        framebuffer.g2.begin(0xffeeeeee);
         for(rectangle in _rectangles) {
             rectangle.render(framebuffer);
         }
@@ -99,4 +95,30 @@ class Main {
 	}
 
     private static var _rectangles :Array<Rectangle>;
+}
+
+class Rectangle
+{
+    public var x :Variable;
+    public var y :Variable;
+
+    public var width :Variable;
+    public var height :Variable;
+
+    public var color :Int;
+
+    public function new(color :Int) : Void
+    {
+        this.color = color;
+        x = new Variable();
+        y = new Variable();
+        width = new Variable();
+        height = new Variable();
+    }
+
+    public function render(framebuffer :kha.Framebuffer) : Void
+    {
+        framebuffer.g2.color = this.color;
+        framebuffer.g2.fillRect(x.m_value, y.m_value, width.m_value, height.m_value);
+    }
 }
